945e9012d5a94e1192c21c265d5b243b7ea614dd
Nishant Gupta <shub002gupta@gmail.com>
2020-05-12 01:04:14 +0530

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Nishant Gupta on 12/05/20.
+//  Copyright © 2020 Nishant Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
